0f5529228e464011b4752082c215b419ee14a6d7
Marta <marta.janas92@gmail.com>
2022-12-13 14:43:52 +0100

    day 8

--- Day8/day8.py
+++ Day8/day8.py
@@ -37,6 +37,37 @@ def check_visibility(input_data: list,
                     v_grid[i][row_number] = 1
 
 
+def calc_scenic_score(input_data: list,
+                      axis: int,
+                      ):
+    assert axis in (0,1), "Axis must be 0 or 1"
+
+    scores = {}
+    for row_number, row in enumerate(input_data):
+        for i in range(len(row)):
+            current_tree = row[i]
+            score_right = 0
+            score_left = 0
+            for j in range(i+1, len(row)):
+                next_tree_right = row[j]
+                score_right += 1
+                if next_tree_right >= current_tree:
+                    break
+
+            for j in range(i, 0, -1):
+                next_tree_left = row[j-1]
+                score_left += 1
+                if next_tree_left >= current_tree:
+                    break
+
+            if axis == 0:
+                scores[f'{row_number}_{i}'] = score_left*score_right
+            else:
+                scores[f'{i}_{row_number}'] = score_left*score_right
+
+    return scores
+
+
 if __name__ == "__main__":
     input_file_name = sys.argv[1]
     data = read_file(input_file_name)
@@ -51,6 +82,15 @@ if __name__ == "__main__":
 
     total_sum = sum([sum(x) for x in visibility_grid])
     print(f"Number of visible trees: {total_sum}")
+
+    horizontal_visibility = calc_scenic_score(tree_rows, 0)
+    vertical_visibility = calc_scenic_score(tree_cols, 1)
+
+    summary_score = {}
+    for k in horizontal_visibility:
+        summary_score[k] = horizontal_visibility[k] * vertical_visibility[k]
+
+    print(max(summary_score.values()))
 
 
 
